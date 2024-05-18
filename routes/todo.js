6d93a7309e7934c16ca8d178f7070a67c4d9911b
Cassandra <c.obiagwu@ignaite.de>
2024-05-18 23:55:58 +0200

    add sqlite db to server and rewrite functions to post and retrieve data from db

--- routes/todo.js
+++ routes/todo.js
@@ -1,62 +1,96 @@
 
 import express from 'express';
+import sqlite3 from 'sqlite3';
+import { dirname, join } from 'path';
+import { fileURLToPath } from 'url';
 
+const __dirname = dirname(fileURLToPath(import.meta.url));
+const dbPath = join(__dirname, '..', 'db', 'todo.db');
+const db = new sqlite3.Database(dbPath);
 const router = express.Router();
 
-let todos = [
-    {id: 1, title: 'Clean room', completed: true},
-    {id: 2, title: 'Grocery Shopping', completed: false},
-    {id: 3, title: 'Study', completed: true},
-];
+let todos = [];
+db.all('SELECT * FROM todos', (err, rows) => {
+    if (err) {
+        return res.status(500).json({ message: 'Failed to retrieve todos', error: err.message });
+    };
+    todos = rows;
+});
 
 router.get('/', (req, res) => {
-    if (!todos) {
-        return res.status(404).json({message: 'Not Found'});
-    }
-    return res.status(200).json(todos);
+    db.all('SELECT * FROM todos', (err, rows) => {
+        if (err) {
+            return res.status(500).json({ message: 'Failed to retrieve todos', error: err.message });
+        };
+        return res.status(200).json(rows);
+    });
 })
 
 router.get('/:id', (req, res) => {
     const id = parseInt(req.params.id);
-    const todo = todos.find((todo) => todo.id === id);
-    if (!todo) {
-        return res.status(404).json({message: `post with id ${id} is not found`});
-    }
-    return res.status(200).json(todo)
+    db.get('SELECT * FROM todos WHERE id = ?', [id], (err, row) => {
+        if (err) {
+            return res.status(404).json({ error: err.message });
+        }
+        return res.status(201).json(row);
+    });
 })
 
 router.post('/', (req, res) => {
-    const newTodo = {
-        id: todos.length + 1,
-        title: req.body.title,
-        completed: req.body.completed
-    };
-    if (!newTodo.title) {
-        return res.status(400).json({message: 'please include a title'});
+    const { title, completed } = req.body;
+    if (!title) {
+        return res.status(400).json({ message: 'Please include a title' });
     }
-    todos.push(newTodo);
-    return res.status(200).json(todos);
-})
+    const newTodo = {
+        title: title,
+        completed: completed
+    };
+    db.run('INSERT INTO todos (title, completed) VALUES (?, ?)', [newTodo.title, newTodo.completed], function (err) {
+        if (err) {
+            return res.status(500).json({ message: 'Failed to add todo', error: err.message });
+        }
+        db.all('SELECT * FROM todos', (err, rows) => {
+            if (err) {
+                return res.status(500).json({ message: 'Failed to retrieve todos', error: err.message });
+            };
+            return res.status(200).json(rows);
+        });
+    });
+});
 
 router.put('/:id', (req, res) => {
     const id = parseInt(req.params.id);
-    const todo = todos.find((todo) => todo.id === id);
-    if (!todo) {
-        return res.status(404).json({message: `post with id ${id} is not found`});
-    }
-    todo.title = req.body.title ? req.body.title : todo.title;
-    todo.completed = req.body.completed;
-    return res.status(200).json(todo);
+    const { title, completed } = req.body;
+    const newTodo = {
+        title: title,
+        completed: completed
+    };
+    db.run('UPDATE todos SET title = ?, completed = ? WHERE id = ?', [newTodo.title, newTodo.completed, id], (err) => {
+        if (err) {
+            return res.status(404).json({ error: err.message });
+        };
+        db.all('SELECT * FROM todos', (err, rows) => {
+            if (err) {
+                return res.status(500).json({ message: 'Failed to retrieve todos', error: err.message });
+            };
+            return res.status(200).json(rows);
+        });
+    });
 })
 
 router.delete('/:id', (req, res) => {
     const id = parseInt(req.params.id);
-    const todo = todos.find((todo) => todo.id === id);
-    if (!todo) {
-        return res.status(404).json({message: `post with id ${id} is not found`});
-    }
-    todos = todos.filter((todo) => todo.id !== id);
-    return res.status(200).json(todos);
+    db.run('DELETE FROM todos WHERE id = ?', [id], (err) => {
+        if (err) {
+            return res.status(404).json({ error: err.message });
+        };
+        db.all('SELECT * FROM todos', (err, rows) => {
+            if (err) {
+                return res.status(500).json({ message: 'Failed to retrieve todos', error: err.message });
+            };
+            return res.status(200).json(rows);
+        });
+    });
 })
 
 export default router;
